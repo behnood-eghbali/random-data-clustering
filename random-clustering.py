027926075339b4467bbee01b410e629213ffb19e
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2020-12-16 21:21:47 +0330

    Update random-clustering.py

--- random-clustering.py
+++ random-clustering.py
@@ -18,3 +18,7 @@ for letter in ascii_uppercase:
     patterns.extend(repeat(patterns[0],2))
     random_patterns = random.sample(patterns, len(patterns))
 print(random_patterns)
+
+# output:
+# ['ABCDEFGHIJKL', 'A', 'A', 'A', 'ABCDEFGHIJ', 'A', 'A', 'A', 'A', 'ABCDEFGHI', 'ABCDEFGHIJKLMNOPQRS', 'A', 'ABCDEFGHIJKLMNOPQRST', 'A', 'A', 'A', 'A', 'ABCDEFG', 'A', 'ABCDEFGH', 'A', 'A', 'ABCDEFGHIJKLMNOPQR', 'ABCDEFGHIJKLMNOPQRSTU', 'A', 'A', 'ABCDEF', 'A', 'ABCDE', 'ABCDEFGHIJKLMNO', 'A', 'ABCDEFGHIJKLMNOPQRSTUVWXY', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'ABCDEFGHIJKLM', 'A', 'ABCDEFGHIJKLMNOP', 'A', 'ABCD', 'AB', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQRSTUVWX', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQRSTUVW', 'A', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQ', 'A', 'A', 'A', 'ABCDEFGHIJKLMN', 'A', 'A', 'ABCDEFGHIJK', 'ABCDEFGHIJKLMNOPQRSTUV', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'ABC']
+
