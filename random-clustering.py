a1235d37ddca5b199b7f3e9d57dd8f625d498e54
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2020-12-17 22:00:31 +0330

    Update random-clustering.py

--- random-clustering.py
+++ random-clustering.py
@@ -9,6 +9,7 @@ Created on Sat Nov 21 12:03:08 2020
 from string import ascii_uppercase
 from itertools import repeat
 import random
+import pandas as pd
 
 patterns = []
 pattern = ''
@@ -22,3 +23,8 @@ print(random_patterns)
 # output:
 # ['ABCDEFGHIJKL', 'A', 'A', 'A', 'ABCDEFGHIJ', 'A', 'A', 'A', 'A', 'ABCDEFGHI', 'ABCDEFGHIJKLMNOPQRS', 'A', 'ABCDEFGHIJKLMNOPQRST', 'A', 'A', 'A', 'A', 'ABCDEFG', 'A', 'ABCDEFGH', 'A', 'A', 'ABCDEFGHIJKLMNOPQR', 'ABCDEFGHIJKLMNOPQRSTU', 'A', 'A', 'ABCDEF', 'A', 'ABCDE', 'ABCDEFGHIJKLMNO', 'A', 'ABCDEFGHIJKLMNOPQRSTUVWXY', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'ABCDEFGHIJKLM', 'A', 'ABCDEFGHIJKLMNOP', 'A', 'ABCD', 'AB', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQRSTUVWX', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQRSTUVW', 'A', 'A', 'A', 'A', 'A', 'ABCDEFGHIJKLMNOPQ', 'A', 'A', 'A', 'ABCDEFGHIJKLMN', 'A', 'A', 'ABCDEFGHIJK', 'ABCDEFGHIJKLMNOPQRSTUV', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'ABC']
 
+patterns_index = list(range(len(random_patterns)))
+
+d = { 'index': patterns_index, 'list': random_patterns }
+
+df = pd.DataFrame(d)
